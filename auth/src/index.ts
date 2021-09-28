de370ae21a11c7493852b9bf366e3852fea9c361
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2021-09-28 10:49:16 +0530

    Added Db connection in index.ts file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,29 +1,41 @@
 import express from 'express'
 import { json } from 'body-parser'
 
-import 'express-async-errors';
+import 'express-async-errors'
 
-import { CurrentUserRoute } from './routes/current-user';
-import { SigninUserRoute } from './routes/signin';
-import { SignoutUserRoute } from './routes/signout';
-import { SignupUserRoute } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { CurrentUserRoute } from './routes/current-user'
+import { SigninUserRoute } from './routes/signin'
+import { SignoutUserRoute } from './routes/signout'
+import { SignupUserRoute } from './routes/signup'
+import { errorHandler } from './middleware/error-handler'
+import { NotFoundError } from './errors/not-found-error'
+import mongoose from 'mongoose'
 
 const app = express()
-app.use(json());
+app.use(json())
 
-app.use(SignupUserRoute);
-app.use(SigninUserRoute);
-app.use(SignoutUserRoute);
-app.use(CurrentUserRoute);
+app.use(SignupUserRoute)
+app.use(SigninUserRoute)
+app.use(SignoutUserRoute)
+app.use(CurrentUserRoute)
 
-app.all('*', async (req,res)=>{
-    throw new NotFoundError();
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
 })
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-app.listen(3000, () => {
-    console.log("Listning at port 3000");
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to database...')
+  } catch (err) {
+    console.error(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listning at port 3000')
+  })
+}
+
+start()
